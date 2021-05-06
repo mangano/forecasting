3123bb0062843e7b2b8ef77a1803f0e63a7367c8
Boris Mangano <boris.mangano@gmail.com>
2021-05-06 23:24:47 +0200

    plotly with dataframes

--- forecasting/bplot.py
+++ forecasting/bplot.py
@@ -1,14 +1,23 @@
 from IPython.display import display, HTML, Markdown
 import plotly.graph_objects as go
+import pandas as pd
 
 import plotly.io as pio
 pio.templates.default = "none"
 
-def plot_timeseries(traces, labels, title='', xlabel='', ylabel='',
+def plot_timeseries(data, labels, title='', xlabel='', ylabel='',
                     shapes=None, hoverformat = '.2f',
                     mode='lines+markers',
                     legendx=None, legendy=1.1,
                     x_range=None,y_range=None,width=None,height=None, visibility=None):
+
+    if(type(data) == pd.DataFrame):
+        traces = []
+        for c in data.columns:
+            traces.append(data[c])        
+    else:
+        traces = data
+    
     if visibility==None:
         visible_traces=[True]*len(labels)
     else:
